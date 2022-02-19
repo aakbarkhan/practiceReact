a8f0e584b53feb3ce3b8ad68b194261f7b3b1fde
aakbarkhan <aakbarkhan@gmail.com>
2022-02-19 22:08:26 +0530

    iniital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { TestContainer } from "../src/TestContainer/TestContainer";
+
+import { typingTestData } from '../src/data/source'
+import { randomElementSelector } from '../src/helper/randomSelector';
+import './App.css';
+import { testDetailsCalculator } from '../src/helper/testDetailsCalculator';
+
+const defaultState = {
+	timerStarted: false,
+	timeRemaining: 60,
+	detailsData: {
+		words: 0,
+		characters: 0,
+		mistakes: 0,
+	},
+	selectedParagraph: ''
+}
+
+class App extends React.Component {
+	constructor () {
+		super();
+
+		this.state = defaultState
+	}
+
+	componentDidMount = () => {
+		console.log('Selecting Paragraph...');
+		this.setState({
+			...this.state,
+			selectedParagraph: randomElementSelector(typingTestData)
+		})
+	}
+
+	handleKeyPress = (inputValue) => {
+		if (!this.state.timerStarted) this.startTimer()
+
+		const updatedDetails = testDetailsCalculator(this.state.selectedParagraph, inputValue)
+		console.log('Updated Details: ', updatedDetails)
+
+		this.setState({ detailsData: updatedDetails })
+	}
+
+	startAgain = () => this.setState({ ...defaultState, selectedParagraph: randomElementSelector(typingTestData)})
+
+	startTimer = () => {
+		this.setState({ timerStarted: true });
+		const timer = setInterval(() => {
+			if (this.state.timeRemaining > 0) {
+				this.setState({ timeRemaining: (this.state.timeRemaining - 1) })
+			} else {
+				clearInterval(timer)
+			}
+		}, 1000)
+	}
+
+	render() {
+		return (
+			<div className="app-container">
+				<h1 className="main-heading">Speedy Type</h1>
+				<div className="test-container-main">
+					<TestContainer
+						handleKeyPress={this.handleKeyPress}
+						timeRemaining={this.state.timeRemaining}
+						timerStarted={this.state.timerStarted}
+						startAgain={this.startAgain}
+						words={this.state.detailsData.words}
+						characters={this.state.detailsData.characters}
+						mistakes={this.state.detailsData.mistakes}
+						selectedParagraph={this.state.selectedParagraph}
+					/>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default App;
